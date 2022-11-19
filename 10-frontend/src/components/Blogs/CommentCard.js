c1d3ccb9c7e9dfafd20cabf4d6f8214f69073308
Umakant Vashishtha <41837826+umakantv@users.noreply.github.com>
2022-11-19 16:14:26 +0530

    Add prettier config

--- 10-frontend/src/components/Blogs/CommentCard.js
+++ 10-frontend/src/components/Blogs/CommentCard.js
@@ -1,26 +1,30 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
-import CardActions from '@mui/material/CardActions';
-import { getRelativeTime } from '../Utils/Timestamp';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import CardActions from "@mui/material/CardActions";
+import { getRelativeTime } from "../Utils/Timestamp";
 
-export default function CommentCard({comment}) {
+export default function CommentCard({ comment }) {
   return (
-    <Card style={{marginTop: 10}} variant='outlined'>
-        <CardContent>
-        <CardActions style={{ alignItems: "flex-end" }}>
-          <Avatar alt={comment.author.name} src={comment.author.image} />
+    <Card style={{ marginTop: 10 }} variant="outlined">
+      <CardContent>
+        <CardActions style={{ alignItems: "center" }}>
+          <Avatar
+            style={{ width: 40 }}
+            alt={comment.author.name}
+            src={comment.author.image}
+          />
           <Typography gutterBottom variant="h5" component="div">
             {comment.author.name}
           </Typography>
-          </CardActions>
-          <Typography variant="body2" color="text.secondary">
-            {comment.content} <br />
-            {getRelativeTime(comment.createdAt)}
-          </Typography>
-        </CardContent>
+        </CardActions>
+        <Typography variant="body2" color="text.secondary">
+          {comment.content} <br />
+          {getRelativeTime(comment.createdAt)}
+        </Typography>
+      </CardContent>
     </Card>
   );
 }

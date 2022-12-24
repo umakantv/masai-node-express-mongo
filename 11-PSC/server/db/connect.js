7074e0a1d864260e6821e4bd93cd5fa1aa0abee6
Umakant Vashishtha <41837826+umakantv@users.noreply.github.com>
2022-12-24 20:28:29 +0530

    Remove unsecure log

--- 11-PSC/server/db/connect.js
+++ 11-PSC/server/db/connect.js
@@ -5,8 +5,6 @@ const config = require('../config');
 async function connect() {
     // protocol://username:password@host:port/db_name
 
-    console.log(config.DB_CONNECTION_URL)
-
     return new Promise((resolve, reject) => {
         mongoose.connect(config.DB_CONNECTION_URL, (err) => {
             if (err) {

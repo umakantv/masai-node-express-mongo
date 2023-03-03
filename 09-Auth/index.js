42498dc2a31ed638ec6e541f21d872c022eaf81a
Umakant Vashishtha <41837826+umakantv@users.noreply.github.com>
2023-03-03 20:18:19 +0530

    Serve index.html as fallback

--- 09-Auth/index.js
+++ 09-Auth/index.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
+const path = require('path')
 const connectDatabase = require('./db/connectDatabase');
 const authRouter = require('./routes/auth.routes');
 const postRouter = require('./routes/posts.routes');
@@ -15,7 +16,7 @@ const app = express();
 // Apply middleware functions
 app.use(express.json()); // Middleware defined by express to convert req body into json object
 
-app.use(express.static('static'));
+app.use(express.static('public'));
 // app.use(logRequest);
 app.use(morgan('tiny'));
 app.use(cors());
@@ -26,7 +27,11 @@ app.use('/api/auth', authRouter);
 app.use('/api/comment', commentRouter);
 app.use('/api/user', userRouter);
 
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+})
+
 connectDatabase()
 .then(() => {
-    app.listen(3001, () => console.log('Server listening on http://localhost:3001'));
+    app.listen(3000, () => console.log('Server listening on http://localhost:3001'));
 })

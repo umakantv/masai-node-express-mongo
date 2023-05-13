13a973781a1321bf75843a77c87a54082a574f16
Umakant Vashishtha <41837826+umakantv@users.noreply.github.com>
2023-05-13 14:23:32 +0530

    DB Connect URL

--- 09-Auth/db/connectDatabase.js
+++ 09-Auth/db/connectDatabase.js
@@ -2,6 +2,8 @@ import { connect } from 'mongoose';
 
 export default async function connectDatabase() {
 
-    await connect('mongodb://127.0.0.1:27017/auth-10');
+    let DB_CONNECTION_URL = process.env.DB_CONNECTION_URL;
+
+    await connect(DB_CONNECTION_URL);
     console.log('Connected to the database');
 }
